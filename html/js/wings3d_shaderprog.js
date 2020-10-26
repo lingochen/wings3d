8cc5f0bd3040cf13c4cad379e79015f595b81593
Lingo Chen <boeing.chen@gmail.com>
2020-10-26 13:59:34 +0800

    Refactor pbr sphere shading code. mimic Wings3D reference implementation.
    Why: many pbr functions. to be as close as possible to the reference implementation
    
    Tags: PbrSphere Material

--- html/js/wings3d_shaderprog.js
+++ html/js/wings3d_shaderprog.js
@@ -434,31 +434,217 @@ let pbrMaterial = {
    vertex:
       `attribute vec3 position;
       attribute vec3 normal;
+      uniform vec3 baseColor;
       
-      uniform mat4 worldView;
-      uniform mat4 projection;
+      uniform mat4 world;
+      uniform mat4 worldViewProjection;
 
-      varying vec3 vNormal;
+      varying vec3 wsNormal;
+      varying vec3 wsPosition;
+      varying vec3 vBaseColor;
+      varying vec2 vTexCoord;
       void main(void) {
-         vNormal = normal;
-         gl_Position = projection * worldView * vec4(position, 1.0);
+         wsNormal = (world * vec4(normal, 1.0)).xyz;
+         wsPosition = (world * vec4(position, 1.0)).xyz;
+         gl_Position = worldViewProjection * vec4(position, 1.0);
+         vBaseColor = baseColor;
+         vTexCoord = vec2(0.0);
       }`,
-   fragment:
+   fragment: (base, material) =>
       `precision mediump float;
 
-      struct Material {
-         vec3 baseColor;
-      };
+      ${base}
+      // material value is here
+      ${material}   
 
-      uniform Material mtl;
+      varying vec3 wsNormal; 
+      varying vec3 wsPosition;
+      uniform vec3 wsEyepoint;
       uniform vec3 lightDir;
-      varying vec3 vNormal; 
+      //uniform vec4 wsLightpos;
+      uniform vec3 lightDiffuse;
+      uniform vec3 lightSpecular;
+      //uniform vec3 lightAtt;
 
       void main(void) {
-         float diff = max(dot(normalize(vNormal), lightDir), 0.0);
-         gl_FragColor = vec4(diff * mtl.baseColor, 1);
+         vec3 n = normalize(wsNormal); //getNormal();
+         vec3 v = normalize(wsEyepoint-wsPosition);  // point to camera
+         PBRInfo pbr = calcViews(n, v, lightDir);
+         pbr = calcMaterial(pbr);
+
+         // Calculate the shading terms for the microfacet specular shading model
+         vec3  F = specularReflection(pbr);
+         float G = geometricOcclusion(pbr);
+         float D = microfacetDistribution(pbr);
+
+         // Calculation of analytical lighting contribution
+         vec3 diffuseContrib = (1.0 - max(max(F.r, F.g), F.b)) * diffuse(pbr);
+         diffuseContrib *= lightDiffuse;
+         vec3 specContrib = F * G * D / (4.0 * pbr.NdotL * pbr.NdotV);
+         specContrib *= lightSpecular;
+         // Obtain final intensity as reflectance (BRDF) scaled by the energy of the light (cosine law)
+         vec3 frag_color = pbr.NdotL * (diffuseContrib + specContrib) 
+         frag_color +=  getEmission();
+         gl_FragColor = vec4(pow(frag_color,vec3(1.0/2.2)), pbr.opaque);
       }`,
 };
+
+
+// we will take the pbr shading calculation from wings3d to be compatible
+// Author: Dan Gudmundsson
+function createPbr() {
+   const base = 
+      `const float M_PI = 3.141592653589793;
+
+      // Encapsulate the various inputs used by the various functions in the shading equation
+      struct PBRInfo {
+         float NdotL;                  // cos angle between normal and light direction
+         float NdotV;                  // cos angle between normal and view direction
+         float NdotH;                  // cos angle between normal and half vector
+         float LdotH;                  // cos angle between light direction and half vector
+         float VdotH;                  // cos angle between view direction and half vector
+         float perceptualRoughness;    // roughness value, as authored by the model creator (input to shader)
+         float metalness;              // metallic value at the surface
+         vec3 reflectance0;            // full reflectance color (normal incidence angle)
+         vec3 reflectance90;           // reflectance color at grazing angle
+         float alphaRoughness;         // roughness mapped to a more linear change in the roughness (proposed by [2])
+         vec3 diffuseColor;            // color contribution from diffuse lighting
+         vec3 specularColor;           // color contribution from specular lighting
+         float occlusion;
+         float opaque;
+      };
+
+      vec4 SRGBtoLINEAR(vec4 srgbIn) {
+         vec3 bLess = step(vec3(0.04045),srgbIn.xyz);
+         vec3 linOut = mix( srgbIn.xyz/vec3(12.92), pow((srgbIn.xyz+vec3(0.055))/vec3(1.055),vec3(2.4)), bLess );
+         return vec4(linOut,srgbIn.w);
+      }
+
+      PBRInfo calcViews(vec3 Norm, vec3 View, vec3 Ligth) {
+         PBRInfo pbr;
+         vec3 Half = normalize(Ligth+View);           // halfv between l and v
+         pbr.NdotL = clamp(dot(Norm, Ligth), 0.001, 1.0);
+         pbr.NdotV = abs(dot(Norm, View)) + 0.001;
+         pbr.NdotH = clamp(dot(Norm, Half), 0.0, 1.0);
+         pbr.LdotH = clamp(dot(Ligth, Half), 0.0, 1.0);
+         pbr.VdotH = clamp(dot(View, Half), 0.0, 1.0);
+         return pbr;
+      }
+
+      // The following equation models the Fresnel reflectance term of the spec equation (aka F())
+      // Implementation of fresnel from [4], Equation 15
+      vec3 specularReflection(PBRInfo pbrInputs){
+         return pbrInputs.reflectance0 +
+            (pbrInputs.reflectance90 - pbrInputs.reflectance0)
+            * pow(clamp(1.0 - pbrInputs.VdotH, 0.0, 1.0), 5.0);
+      }
+
+      // This calculates the specular geometric attenuation (aka G()),
+      // where rougher material will reflect less light back to the viewer.
+      // This implementation is based on [1] Equation 4, and we adopt their modifications to
+      // alphaRoughness as input as originally proposed in [2].
+      float geometricOcclusion(PBRInfo pbrInputs) {
+         float NdotL = pbrInputs.NdotL;
+         float NdotV = pbrInputs.NdotV;
+         float r = pbrInputs.alphaRoughness*pbrInputs.alphaRoughness;
+
+         float attenuationL = NdotL / (NdotL + sqrt(r + (1.0 - r) * (NdotL * NdotL)));
+         float attenuationV = NdotV / (NdotV + sqrt(r + (1.0 - r) * (NdotV * NdotV)));
+         return 8.0 * attenuationL * attenuationV;
+      }
+
+      // The following equation(s) model the distribution of microfacet normals across the area being drawn (aka D())
+      // Implementation from "Average Irregularity Representation of a Roughened Surface for Ray Reflection" by T. S. Trowbridge, and K. P. Reitz
+      // Follows the distribution function recommended in the SIGGRAPH 2013 course notes from EPIC Games [1], Equation 3.
+      float microfacetDistribution(PBRInfo pbrInputs) {
+         float roughnessSq = pbrInputs.alphaRoughness * pbrInputs.alphaRoughness;
+         float f = (pbrInputs.NdotH * roughnessSq - pbrInputs.NdotH) * pbrInputs.NdotH + 1.0;
+         return roughnessSq / (M_PI * f * f);
+      }
+
+      // Basic Lambertian diffuse
+      // Implementation from Lambert's Photometria https://archive.org/details/lambertsphotome00lambgoog
+      // See also [1], Equation 1
+      vec3 diffuse(PBRInfo pbrInputs) {
+         return pbrInputs.diffuseColor / M_PI;
+      }
+      `;
+   const material = 
+      `
+      uniform int useDiffuseMap;
+      uniform int usePBRMap;
+      uniform int useEmissionMap;
+      
+      uniform sampler2D diffuseMap;
+      uniform sampler2D PBRMap;
+      uniform sampler2D emissionMap;
+      
+      uniform float metallic;
+      uniform float roughness;
+      uniform vec3  emission;
+      
+      varying vec2 vTexCoord;
+      varying vec3 vBaseColor;  // diffuse * vertex_color
+      
+      vec3 getBasecolor() {
+         if (useDiffuseMap > 0) {
+            return vBaseColor*SRGBtoLINEAR(texture2D(diffuseMap, vTexCoord)).rgb;
+         }
+         return vBaseColor;
+      }
+      
+      vec3 getEmission() {
+         if (useEmissionMap > 0) {
+            return SRGBtoLINEAR(texture2D(emissionMap, vTexCoord)).rgb * emission;
+         }
+         return emission;
+      }
+      
+      vec4 getPbrOmr() {  // red = occlusion blue = roughness green = metallic
+         vec4 mrSample = vec4(1.0, roughness, metallic, 1.0);
+         if (usePBRMap > 0) {
+            mrSample *= texture2D(PBRMap, vTexCoord);
+         }
+         return clamp(mrSample, 0.04, 0.96);
+      }
+      
+      float getOcclusion() {
+         if (usePBRMap > 0) {
+            return texture2D(PBRMap, vTexCoord).x;
+         }
+         return 1.0;
+      }
+      
+      PBRInfo calcMaterial(PBRInfo pbr) {
+         vec3 baseColor = getBasecolor();
+         vec3 f0 = vec3(0.04);
+         vec3 omr = getPbrOmr().rgb;
+         float rgh = omr.g;
+         float met = omr.b;
+         vec3 diffuse  = mix(baseColor.rgb, vec3(0.0), met);
+         vec3 specular = mix(f0, baseColor.rgb, met);
+      
+         float reflectance = max(max(specular.r, specular.g), specular.b);
+         float reflectance90 = clamp(reflectance * 25.0, 0.0, 1.0);
+         vec3 specularEnvironmentR0 = specular.rgb;
+         vec3 specularEnvironmentR90 = vec3(1.0, 1.0, 1.0) * reflectance90;
+      
+         pbr.perceptualRoughness = rgh;
+         pbr.alphaRoughness = rgh*rgh;
+      
+         pbr.reflectance0 = specularEnvironmentR0;
+         pbr.reflectance90 = specularEnvironmentR90;
+      
+         pbr.diffuseColor = diffuse;
+         pbr.specularColor = specular;
+         pbr.opaque = 1.0;
+         pbr.occlusion = omr.r;
+         return pbr;
+      }
+      `;
+
+      pbrMaterial = gl.createShaderProgram(pbrMaterial.vertex, pbrMaterial.fragment(base, material));
+}
 
 
 Wings3D.onReady(function() {
@@ -487,7 +673,7 @@ Wings3D.onReady(function() {
 
    drawSelectablePolygon = gl.createShaderProgram(drawSelectablePolygon.vertex(index2TexCoord, materialIndex), drawSelectablePolygon.fragment);
 
-   pbrMaterial = gl.createShaderProgram(pbrMaterial.vertex, pbrMaterial.fragment);
+   createPbr();
 });
 
 export {
